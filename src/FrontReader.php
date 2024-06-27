d0129bf2e52d4443dba6ba8b6c1da2bcb9e68269
Gabriel Novaes <semproblema@gmail.com>
2024-06-27 09:37:44 -0400

    first commit

---
+++ src/FrontReader.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dothnews\FrontReader;
+
+class FrontReader
+{
+    public function hi(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
