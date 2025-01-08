ad51d044e5634669bce1adbac677684ca65559c7
Shiguenori <shisjr@gmail.com>
2025-01-08 17:35:20 -0400

    Remoção dos helpers e page_type de arquivo

--- src/Http/Controllers/Page/IndexPageController.php
+++ src/Http/Controllers/Page/IndexPageController.php
@@ -10,68 +10,30 @@ class IndexPageController extends BaseController
 {
     public function index($slug = 'ultimas-noticias')
     {
-        $page = $this->getPageData($slug);
+        $pageService = new PageService();
+
+        $page = $pageService->getPage($slug);
 
         if (! isset($page->data->id)) {
             abort(404);
         }
 
-        $template = $this->resolveTemplate($page->data->page_type);
-
-        if ($template === 'page.html') {
-            return $this->outputContent($page->data->link, $page->data->content);
-        }
+        $template = $pageService->templateByType($page->data->page_type);
 
         if (! View::exists($template)) {
             abort(404);
         }
 
-        return $this->renderTemplate($template, $slug, $page->data);
-    }
-
-    public function listByPage($slug)
-    {
-        return $this->index($slug);
-    }
-
-    private function getPageData(string $slug)
-    {
-        $pageService = new PageService();
-
-        return $pageService->getPage($slug);
-    }
-
-    private function resolveTemplate(string $pageType): string
-    {
-        $pageService = new PageService();
-
-        return $pageService->templateByType($pageType);
-    }
-
-    private function renderTemplate(string $template, string $slug, $pageData)
-    {
         $data = [
             'slug' => $slug,
-            'page' => $pageData,
+            'page' => $page->data,
         ];
 
         return view($template, $data);
     }
 
-    private function outputContent(string $link, string $content = '')
+    public function listByPage($slug)
     {
-        $contentType = $this->resolveContentType($link);
-
-        return response($content, 200)->header('Content-Type', $contentType);
-    }
-
-    private function resolveContentType(string $link): string
-    {
-        return match (pathinfo($link, PATHINFO_EXTENSION)) {
-            'txt' => 'text/plain',
-            'js' => 'application/javascript',
-            'json' => 'application/json',
-            default => 'text/html',
-        };
+        return $this->index($slug);
     }
 }
