4e3f2ec38b5df1f768afd02f0cb289f5c5c16c04
Gabriel Novaes <semproblema@gmail.com>
2025-01-08 09:28:08 -0400

    readme

--- src/FrontReaderServiceProvider.php
+++ src/FrontReaderServiceProvider.php
@@ -10,8 +10,11 @@ class FrontReaderServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        // The loadRoutesFrom method is used to load routes from a file within your package.
         $this->loadRoutesFrom(__DIR__.'/Http/Routes/web.php');
 
+        // THE publishes METHOD is used to publish the configuration file to the application's config directory,
+        // using command php artisan vendor:publish --tag=front-reader-config
         $this->publishes([
             __DIR__.'/config/front-reader.php' => config_path('front-reader.php'),
         ], 'front-reader-config');
