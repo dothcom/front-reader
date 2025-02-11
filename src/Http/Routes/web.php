728df46bd12d5eb40176729b2d6bd1f724a76f35
Gabriel Novaes <semproblema@gmail.com>
2025-02-11 14:56:28 -0400

    Add debug route to display environment variables for DOTHNEWS API

--- src/Http/Routes/web.php
+++ src/Http/Routes/web.php
@@ -6,6 +6,13 @@ use Dothcom\FrontReader\Http\Controllers\Post\DetailPostController;
 use Dothcom\FrontReader\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
+// debug env vars inline
+Route::get('/_debug', function () {
+    // print env DOTHNEWS_API_BASE_URL
+    echo 'DOTHNEWS_API_BASE_URL: ' . env('DOTHNEWS_API_BASE_URL') . '<br>';
+    echo 'DOTHNEWS_API_VERSION: ' . env('DOTHNEWS_API_VERSION') . '<br>';
+});
+
 Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('/busca', [SearchController::class, 'index'])->name('search.index');
 Route::get('/posts/{slug}', [DetailPostController::class, 'index'])->name('posts.detail');
