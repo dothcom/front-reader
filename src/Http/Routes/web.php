2c0ff842456975de8f6ada8fe9ef0b0d36a28a11
Shiguenori <shisjr@gmail.com>
2025-05-16 11:49:47 -0400

    Pint

--- src/Http/Routes/web.php
+++ src/Http/Routes/web.php
@@ -26,13 +26,13 @@ Route::get('/{slug?}', function ($slug) {
         $slug = end($segments);
     }
     try {
-        $pageService = new PageService;
+        $pageService = new PageService();
         $page = $pageService->getPage($slug);
         if (isset($page->data->id)) {
             return app(IndexPageController::class)->listByPage($slug);
         }
     } catch (NotFoundHttpException $e) {
-        $postService = new PostService;
+        $postService = new PostService();
         $post = $postService->getPostBySlug($slug);
 
         if (isset($post->data->id)) {
