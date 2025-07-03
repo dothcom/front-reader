144241f611de4cc2078a41f80ae5cb2c09dc4891
Gabriel Novaes <semproblema@gmail.com>
2025-07-03 16:58:57 -0400

    fix get permalinks

--- src/Http/Controllers/Post/DetailPostController.php
+++ src/Http/Controllers/Post/DetailPostController.php
@@ -7,10 +7,12 @@ use Dothcom\FrontReader\Services\PostService;
 
 class DetailPostController extends BaseController
 {
-    public function index($slug = 'titulo-do-post')
+    public function index(string $permalink)
     {
+        $permalink = '/' . ltrim($permalink, '/');
+
         $postService = new PostService();
-        $post = $postService->getPostBySlug($slug, [
+        $post = $postService->getPostBySlug($this->slugFromPermalink($permalink), [
             '_embed' => 'featuredmedia,users,categories,tags,medias',
         ]);
 
@@ -18,8 +20,31 @@ class DetailPostController extends BaseController
             return abort(404);
         }
 
+        if ($post->data->permalink !== $permalink) {
+            return abort(404);
+        }
+
+
         $template = $postService->templateByType($post->data->post_type);
 
         return view($template, ['post' => $post->data]);
+    }
+
+    /**
+     * Extracts the slug from a permalink.
+     *
+     * @param  string  $permalink  The permalink to extract the slug from.
+     * @return string The extracted slug.
+     */
+    // Example: /noticia/2025/06/23/voluptas-dolor-labore-molestiae-ex-suscipit-accusamus.html
+    // This method assumes the slug is the last part of the permalink,
+    // excluding the file extension (.html).
+    // returns: 'voluptas-dolor-labore-molestiae-ex-suscipit-accusamus'
+    private function slugFromPermalink(string $permalink): string
+    {
+        $parts = explode('/', $permalink);
+        $slug = end($parts);
+
+        return preg_replace('/\.html$/', '', $slug);
     }
 }
